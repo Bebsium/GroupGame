61172ec29d279f0fee3bbdc47891fbf40a2917e3
Bebsium <bebsium@gmail.com>
2019-05-20 15:40:45 +0900

    Add 摄像机从场景中获得Player物体

--- Assets/Script/Camera/CameraSetting.cs
+++ Assets/Script/Camera/CameraSetting.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class CameraSetting : MonoBehaviour
 {
+    public GameObject[] players;
     // Start is called before the first frame update
     void Start()
     {
         //Test
+        players = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
