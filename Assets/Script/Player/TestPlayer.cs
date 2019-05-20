9992e00e0cb3b388f78c36268812993dabbe8dd4
NanaVoice <40158778+NanaVoice@users.noreply.github.com>
2019-05-20 16:05:07 +0900

    Fixed Player Moving

--- Assets/Script/Player/TestPlayer.cs
+++ Assets/Script/Player/TestPlayer.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,11 +15,13 @@ public class TestPlayer : Controller
 
     //可使用函数
     //SetFaction(nowFaction)            --重设玩家阵营
-
+    private CharacterController cc;
+    public float speed = 0;
     public Global.Faction nowFaction;
 
     protected override void Start()
     {
+        cc = GetComponent<CharacterController>();
         base.Start();
         Rigi.freezeRotation = true;
     }
@@ -33,10 +35,42 @@ public class TestPlayer : Controller
     protected override void Move()
     {
         //重写移动
-        base.Move();
+        if(Faction == Global.Faction.Player1)
+        {
+            float h=0, v=0;
+            if (Input.GetKey(Global.Key.Forward))
+            {
+                v = 1;
+            }else if (Input.GetKey(Global.Key.Back))
+            {
+                v = -1;
+            }
+
+            if (Input.GetKey(Global.Key.Right))
+            {
+                h = 1;
+            }else if (Input.GetKey(Global.Key.Left))
+            {
+                h = -1;
+            }
+            
+            //float h = Input.GetAxis("Horizontal");
+            //float v = Input.GetAxis("Vertical");
+            if (Mathf.Abs(h) > 0.1f || Mathf.Abs(v) > 0.1)
+            {
+                Vector3 targetDir = new Vector3(h, 0, v);
+                transform.LookAt(targetDir + transform.position);
+                cc.SimpleMove(transform.forward * speed);
+            }
+        }else if(Faction == Global.Faction.Player2)
+        {
+
+        }
+        
+        //base.Move();
     }
 
-    protected override void Jump()
+protected override void Jump()
     {
         //重写跳跃
         base.Jump();
