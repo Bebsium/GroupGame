4d47abbbb183ccb6f6227e618d82dd46e01122bf
drthxz <44692848+drthxz@users.noreply.github.com>
2019-05-20 16:07:54 +0900

    Add Item script

---
+++ Assets/Script/Item/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public GameObject itemAreaPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemAreaPrefab = Resources.Load<GameObject>("Prefab/Item/ItemArea");
+        Instantiate(itemAreaPrefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+ 
+        if (other.gameObject.tag == "Untagged")
+        {
+            
+            if (Input.GetKey(KeyCode.G))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
