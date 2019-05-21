883fe1e6a9397e3d48a427142e92c574fd85e806
Bebsium <bebsium@gmail.com>
2019-05-21 18:41:55 +0900

    Add 添加ReadMe说明文件

---
+++ Assets/Script/ReadMe/ReadMe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu]
+public class ReadMe : ScriptableObject
+{
+    public Texture2D icon;
+    public float iconMaxWidth = 128f;
+    public string title;
+    public Section[] sections;
+
+    [Serializable]
+    public class Section
+    {
+        public string heading, linkText, url;
+        public string[] text;
+    }
+}
