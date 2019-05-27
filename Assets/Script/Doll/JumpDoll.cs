20991b47a9599249c1f17d89b500dd9ade6913e5
jilk432100 <50429837+jilk432100@users.noreply.github.com>
2019-05-27 16:20:07 +0900

    Fixed Jump

---
+++ Assets/Script/Doll/JumpDoll.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpDoll : Doll
+{
+    public GameObject groundCheck;
+    public Rigidbody2D playerRd;
+    public bool canJump = true;
+    public float jumpForce;
+
+
+    public override bool PickItem(string name)
+    {
+        print(Owner.Faction.ToString() + " picked " + name);
+        return true;
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        Rigi.freezeRotation = true;
+    }
+
+    protected override void Loop()
+    {
+        //相当于Update
+        if ((Input.GetKey(KeyCode.Alpha1) && Owner.Faction == Global.Faction.Player1)
+            || (Input.GetKey(KeyCode.KeypadDivide) && Owner.Faction == Global.Faction.Player2))
+        {
+            Hurt = 1f;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Alpha2) && Owner.Faction == Global.Faction.Player1)
+            || (Input.GetKeyDown(KeyCode.KeypadMultiply) && Owner.Faction == Global.Faction.Player2))
+        {
+            GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            temp.transform.position = transform.position;
+            temp.name = "Putted Item";
+            temp.AddComponent<Item>().picked = true;
+        }
+    }
+
+    protected override void Move()
+    {
+        //重写移动
+        base.Move();
+    }
+
+    protected override void Jump()
+    {
+        //重写跳
+        if (canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigi.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
+        }
+        
+
+    }
+    //private bool IsJump()
+    //{
+    //    bool isjump = false;
+    //    Physics.Linecast(transform.position-Vector3.up,)
+    //    isjump= Physics2D.Linecast(playerRd.transform.position, groundCheck.transform.position, 1 << LayerMask.NameToLayer("Ground"));
+    //    Debug.Log(isjump);
+    //    return isjump;
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if(other.tag == )
+        canJump = true;
+    }
+
+}
---
+++ Assets/Script/Player/JumpPlayer.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class JumpPlayer : Controller
+{
+    //可使继承用参数
+    //public Global.Faction Faction     --玩家阵营
+    //                                      Global.Faction.Player1
+    //                                      Global.Faction.Player2
+    //                                      Global.Faction.Computer
+    //public bool hasDoll               --玩家是否拥有人偶
+    //protected Rigidbody Rigi;         --Rigibody
+
+    //可使用函数
+    //SetFaction(nowFaction)            --重设玩家阵营
+    public float gra = 8;
+    public bool canJump = true;
+    public float jumpForce;
+    public Global.Faction nowFaction;
+
+    protected override void Start()
+    {
+        base.Start();
+        SetFaction(nowFaction);
+        Rigi.freezeRotation = true;
+    }
+
+    protected override void Loop()
+    {
+        //相当于Update
+        SetFaction(nowFaction);
+        Rigi.AddForce(-Vector3.up * gra);
+        print("Gravity force "+ gra);
+    }
+
+    protected override void Move()
+    {
+        ////重写移动
+        //if(Faction == Global.Faction.Player1)
+        //{
+        //    float h=0, v=0;
+        //    if (Input.GetKey(Global.Key.Forward))
+        //    {
+        //        v = 1;
+        //    }else if (Input.GetKey(Global.Key.Back))
+        //    {
+        //        v = -1;
+        //    }
+
+        //    if (Input.GetKey(Global.Key.Right))
+        //    {
+        //        h = 1;
+        //    }else if (Input.GetKey(Global.Key.Left))
+        //    {
+        //        h = -1;
+        //    }
+
+        //    float y = Camera.main.transform.rotation.eulerAngles.y;
+        //    Vector3 targetDir = new Vector3(h, 0, v).normalized;
+        //    Vector3 tempTarget = Vector3.Lerp(transform.forward, targetDir, 0.1f);
+        //    float spd = Time.deltaTime * Global.Parameter.SoulSPD;
+        //    if (Vector3.Dot(targetDir, tempTarget)<0)
+        //    {
+        //        spd *= 0.2f;
+        //    }
+        //    tempTarget = Quaternion.Euler(0, y, 0) * tempTarget;
+        //    //transform.LookAt(tempTarget + transform.position);
+        //    transform.Translate(targetDir * spd, Space.World);
+
+        //}else if(Faction == Global.Faction.Player2)
+        //{
+
+        //}
+
+        base.Move();
+    }
+
+    protected override void Jump()
+    {
+        //重写跳跃
+        if (canJump && Input.GetKeyDown(KeyCode.Space) && Faction == Global.Faction.Player1)
+        {
+            Rigi.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
+        }
+
+        if (canJump && Input.GetKeyDown(KeyCode.Keypad0) && Faction == Global.Faction.Player2)
+        {
+            Rigi.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+}
