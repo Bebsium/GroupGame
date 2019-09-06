c61c58fb6fe1066207d33678a0464c96624ef88f
prprprprp <50429770+prprprprp@users.noreply.github.com>
2019-09-06 22:00:05 +0900

    move complete 1.1

--- Assets/CatMove.cs
+++ Assets/CatMove.cs
@@ -4,55 +4,62 @@ using UnityEngine;
 
 public class CatMove : MonoBehaviour
 {
-    bool alive = true;
-    float speed = 4;
-    float rotSpeed = 82;
-    float gravity = 8;
-    float rot = 0f;
+   
     Vector3 moveDir = Vector3.zero;
     CharacterController controller;
     Animator anim;
-  
-
+ 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
-        anim = GetComponent<Animator>();
-       
+        anim = GetComponent<Animator>();      
     }
     // Update is called once per frame
     void Update()
     {
-        if (!alive)
-            return;
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P)) //落命
         {
             anim.SetBool("die", true);
-            alive = false;
-            
         }
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))//移动+拾取道具
         {
             anim.SetBool("run", true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetBool("carry", true);               
+            }
         }
         else
         {
             anim.SetBool("run", false);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))// 跳
         {
             anim.SetTrigger("jump");
+            Debug.Log("jump");
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))//攻击
         {
             anim.SetTrigger("attack");
+            anim.SetBool("carry", false);
+            Debug.Log("atk");
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))//拾取
         {
-            anim.SetTrigger("throw");
+            anim.SetBool("carry", true);
+            
         }
-
+        if (Input.GetKeyDown(KeyCode.R))//扔
+        {
+            anim.SetBool("carry", false);
+            anim.SetTrigger("throw");      
+            Debug.Log("throw");
+        }
+        if (Input.GetKeyDown(KeyCode.F))//技能
+        {   
+            anim.SetTrigger("Skill");
+            Debug.Log("fuck");
+        }
 
     }
 }
